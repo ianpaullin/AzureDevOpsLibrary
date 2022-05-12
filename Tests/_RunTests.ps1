db35bdb322ebb6efb575da07784d95225f7501c8
ian.paullin <ianpaullin@gmail.com>
2022-05-12 00:41:57 -0500

    Updating _RunTests script runner for Pester tests

--- Tests/_RunTests.ps1
+++ Tests/_RunTests.ps1
@@ -1,8 +1,11 @@
-Import-Module Pester -Passthru
+$pesterVersion = (Import-Module Pester -Passthru).Version 
+
+Write-Host "Running Pester version $($pesterVersion)"
 
 # get all files that are named *.Tests.ps1 
+$testFiles = (Get-ChildItem -Name) -like "*.Tests.ps1" 
+
+Write-Host "Found $($testFiles.count) test files to run."
 
 # execute each file against pester
-
-Invoke-Pester .\Configuration.Tests.ps1
-Invoke-Pester .\Utilities.Tests.ps1
+$testFiles | %{ Invoke-Pester ".\$_" }
