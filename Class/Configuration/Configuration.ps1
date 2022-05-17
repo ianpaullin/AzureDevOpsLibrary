6164f78774e4cceec56a14bcb96f80fda2db2fa6
ian.paullin <ianpaullin@gmail.com>
2022-05-17 00:24:05 -0500

    Configuration class not quite working as expected yet

--- Class/Configuration/Configuration.ps1
+++ Class/Configuration/Configuration.ps1
@@ -1,3 +1,41 @@
 class Configuration {
-    
+    [string]$AzureDevOpsUrl 
+    [string]$PersonalAccessToken
+    [hashtable]$EnvironmentVariables
+
+    Configuration() {
+        # check for environment variables first
+        $tempHt = $this.GetEnvironmentVariables()
+
+        if ($tempHt.Count -gt 0 ) {
+            $this.EnvironmentVariables = $tempHt
+        }
+    }
+
+    [void]SetPersonalAccessToken ( [string]$PAToken ) {
+        $this.PersonalAccessToken = $PAToken 
+    }
+
+    [string]GetAzureDevOpsUrl() {
+        return $this.AzureDevOpsUrl
+    }
+
+    [hashtable]GetEnvironmentVariables() {
+        $envHt = @{}
+
+        return [Hashtable] (((Get-Item -Path Env:) | sort-object -property Name -Descending) | `
+            ForEach-Object { $envHt.Add($_.key, $_.value) })
+    }
+
+    [bool]FindEnvironmentVariable([string]$KeyName) {
+        return $true
+    }
+
 }
+
+$test = [AzureDevOpsConfig]::new()
+
+$test.EnvironmentVariables.Count
+$test.EnvironmentVariables
+
+$test.GetEnvironmentVariables()
